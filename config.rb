002a26a7003f697f46f5505ee2361811c8d0ec0c
Paul Rosania <paul.rosania@gmail.com>
2011-10-27 11:43:02 -0700

    Configure for paul.rosania.org

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/writings/"
+http_images_path = "/writings/images"
+http_fonts_path = "/writings/fonts"
+css_dir = "public/writings/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
